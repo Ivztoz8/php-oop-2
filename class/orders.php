aa3cb2be7c739593d3f0e3fdc5aea6a60620e7cc
Ivan <ivan.toselli31@gmail.com>
2022-05-11 15:51:33 +0200

    Stp 2; Style E-comm page complete!

---
+++ class/orders.php
@@ -0,0 +1,291 @@
+<?php
+
+require __DIR__ . '/users/users.php';
+require __DIR__ . '/products/products.php';
+
+
+?>
+
+<main class="p-3">
+
+    <!-- Ordine 1  -->
+        <div class="container border border-1 border-light mb-5">
+            <div class="row border">
+                <div class="col-1 border text-white">
+                    <p>
+                        order n. : 
+                    </p>
+                </div>
+                <div class="col-9 border"></div>
+                <div class="col-2 border bg-light">
+                    <img src="./assets/img/logoFantAnimalFood.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border text-white">
+                    <p>Name: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>Surname: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>User ID: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                   <p>Address: <span></span></p> 
+                </div>
+                <div class="col border text-white">
+                    <p>Discount: <span></span></p>
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+            </div>
+        </div>
+
+          <!-- Ordine 2  -->
+          <div class="container border border-1 border-light mb-5">
+            <div class="row border">
+                <div class="col-1 border text-white">
+                    <p>
+                        order n. : 
+                    </p>
+                </div>
+                <div class="col-9 border"></div>
+                <div class="col-2 border bg-light">
+                    <img src="./assets/img/logoFantAnimalFood.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border text-white">
+                    <p>Name: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>Surname: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>User ID: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                   <p>Address: <span></span></p> 
+                </div>
+                <div class="col border text-white">
+                    <p>Discount: <span></span></p>
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+            </div>
+        </div>
+
+          <!-- Ordine 3  -->
+          <div class="container border border-1 border-light mb-5">
+            <div class="row border">
+                <div class="col-1 border text-white">
+                    <p>
+                        order n. : 
+                    </p>
+                </div>
+                <div class="col-9 border"></div>
+                <div class="col-2 border bg-light">
+                    <img src="./assets/img/logoFantAnimalFood.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border text-white">
+                    <p>Name: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>Surname: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>User ID: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                   <p>Address: <span></span></p> 
+                </div>
+                <div class="col border text-white">
+                    <p>Discount: <span></span></p>
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+            </div>
+        </div>
+
+          <!-- Ordine 4  -->
+          <div class="container border border-1 border-light mb-5">
+            <div class="row border">
+                <div class="col-1 border text-white">
+                    <p>
+                        order n. : 
+                    </p>
+                </div>
+                <div class="col-9 border"></div>
+                <div class="col-2 border bg-light">
+                    <img src="./assets/img/logoFantAnimalFood.png" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border text-white">
+                    <p>Name: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>Surname: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                    <p>User ID: <span></span></p>
+                </div>
+                <div class="col border text-white">
+                   <p>Address: <span></span></p> 
+                </div>
+                <div class="col border text-white">
+                    <p>Discount: <span></span></p>
+                </div>
+            </div>
+            <div class="row border">
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+                <div class="col border">
+                    <div class="row border text-white"> Product: </div>
+                    <div class="row border text-white"> Pieces: </div>
+                    <div class="row border text-white"> Amount:  </div>
+                    <div class="row border text-white"> Discount:  </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-require __DIR__ . '/class/products/products.php';
-require __DIR__ . '/class/users/users.php'
+
 
 ?>
 
@@ -17,12 +16,46 @@ require __DIR__ . '/class/users/users.php'
     <link rel="stylesheet" href="./assets/css/style.css">
     <title>PHP OOP 2</title>
 </head>
-<body >
-    <header>
-
+<body class="bg-secondary">
+    <header class="border border-2 border-success">
+        <div class="container mb-3">
+            <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-1 py-3 bg-secondary rounded-3 border border-success">
+                        <a href="#"><img src="./assets/img/logoFantAnimalFoodHeader.png" alt="Logo E-commerce" class="img-fluid"></a>
+                    </div>
+                    <div class="col-1 py-3 d-flex">
+                        <img src="./assets/img/avatar_employee.jpg" alt="avatar employee" class="img-fluid border rounded-2">
+                        <div class="row-4 px-3 d-flex border text-white ">
+                            employee n. :   <?php echo rand(1, 600) ?>
+                        </div>
+                        <div class="row-4 px-3 d-flex border text-white ">
+                            hours worked:  <?php echo rand(1, 8760) ?>
+                        </div>
+                        <div class="row-4 px-3 d-flex border text-white ">
+                            holidays accrued: <?php echo rand(1, 4380) ?>
+                        </div>
+                    </div>
+                    <div class="col-1 py-3">
+                        <span class="text-white">Order:</span>
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                            <div class="btn-group me-2" role="group" aria-label="First group">
+                                <button type="button" class="btn btn-success">1</button>
+                                <button type="button" class="btn btn-success">2</button>
+                                <button type="button" class="btn btn-success">3</button>
+                                <button type="button" class="btn btn-success">4</button>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
     </header>
+
+<?php
+
+require __DIR__ . '/class/orders.php';
+
+?>
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    <script src="./assets/js/main.js"></script>   
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script> 
 </body>
 </html>
